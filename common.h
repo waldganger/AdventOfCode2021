85c095fe3ddb2aca1f0618293a26b9a03dbf9531
Antony Merle <antony.merle@univ-pau.fr>
2022-01-20 18:43:10 +0100

    day1ex1 Rust

--- common.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <stdio.h>
-#include <stdint.h>
-#include <string.h>
-#include <stdlib.h>
-#include <inttypes.h>
--- day1.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "common.h"
-
-int day1Ex1(void);
-int day1Ex2(void);
-
