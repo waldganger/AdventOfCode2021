aa54d67aa1c9fb2e65841ae6679e7a494ea5a6fb
Antony Merle <antony.merle@gmail.com>
2022-02-03 22:57:47 +0100

    day 6 part 1 complete

--- day6.cpp
+++ day6.cpp
@@ -4,23 +4,76 @@ void Day6::populateFishes()
 {
 	const std::vector<std::string> fileContent = readFile(m_filePath);
 	const std::vector<std::string> strFishes = splitString(fileContent[0], ",");
+	std::vector<int> initialState;
 
 	for (const std::string& f : strFishes)
 	{
-		m_fishes.push_back(std::stoi(f));
+		initialState.push_back(std::stoi(f));
 	}
+
+	m_fishesStates.push_back(initialState);
 }
 
-void Day6::print()
+void Day6::print() const
 {
-	for (const int& fish : m_fishes)
+	int dayCounter = 0;
+
+	for (const std::vector<int>& dayPop : m_fishesStates)
 	{
-		std::cout << fish << std::endl;
+		if (dayCounter == 0)
+		{
+			std::cout << "Initial state: ";
+		}
+		else
+		{
+			std::cout << "after " << dayCounter << (dayCounter > 1 ? " days " : " day : ") << dayCounter;
+		}
+		for (size_t i = 0; i < dayPop.size(); i++)
+		{
+			std::cout << dayPop[i] << (i == dayPop.size() - 1 ? "" : ",");
+		}
+		dayCounter++;
+		std::cout << std::endl;
 	}
+
+}
+
+int Day6::doFishes(const int days)
+{
+	int dayCounter = 0;
+	while (dayCounter < days)
+	{
+		std::vector<int> newState;
+		int newFishes = 0;
+		for (int fish : m_fishesStates[dayCounter])
+		{
+			switch (fish)
+			{
+			case 0:
+				newState.push_back(6);
+				newFishes++;
+				break;
+			default:
+				newState.push_back(fish - 1);
+				break;
+			}
+		}
+
+		for (size_t i = 0; i < newFishes; i++)
+		{
+			newState.push_back(8);
+		}
+
+		m_fishesStates.push_back(newState);
+		dayCounter++;
+	}
+
+	return m_fishesStates.back().size();
 }
 
 void runDay6()
 {
 	Day6 day6("res/day6.txt");
-	day6.print();
+	const int result = day6.doFishes(80);
+	std::cout << result << std::endl;
 }
--- day6.h
+++ day6.h
@@ -5,15 +5,17 @@
 class Day6
 {
 public:
-	Day6(const char* path) : m_filePath{ path } { populateFishes(); };
+	Day6(const char* path) : m_filePath(path), m_dayCounter(1) { populateFishes(); };
 	~Day6() {};
 
 	void populateFishes();
-	void print();
+	void print() const;
+	int doFishes(const int days);
 
 private:
 	const char* m_filePath;
-	std::vector<int> m_fishes;
+	std::vector<std::vector<int>> m_fishesStates;
+	int m_dayCounter;
 };
 
 void runDay6();
