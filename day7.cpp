9bca4165bc96c0431a3f26fdb1527a0b8c03afba
Antony Merle <antony.merle@gmail.com>
2022-02-11 17:00:20 +0100

    Day7 detectAlignes()

--- day7.cpp
+++ day7.cpp
@@ -2,17 +2,22 @@
 
 
 Day7::Day7(const char* path)
-{
+{ 
+	m_filePath = path;
+	
+	if (m_filePath == NULL) return;
+
 	const std::vector<std::string> lines = readFile(path);
 
 	for (const std::string& line : lines)
 	{
-		const std::vector< std::string> temp = splitString(line, ",");
+		const std::vector<std::string> temp = splitString(line, ",");
 		for (const std::string strNum : temp)
 		{
 			m_positions.push_back(std::stoi(strNum));
 		}
 	}
+
 }
 
 uint32_t Day7::getMean() const
@@ -26,8 +31,38 @@ uint32_t Day7::getMean() const
 	return sum / m_positions.size();
 }
 
+void Day7::detectAlignes() 
+{
+	uint32_t max;
+
+	for (size_t i = 0; i < m_positions.size(); i++)
+	{
+		int counter = 0;
+		for (size_t j = i; j < m_positions.size(); j++)
+		{
+			if (m_positions[j] == m_positions[i])
+			{
+				counter++;
+			}
+		}
+		m_occurences.push_back({ m_positions[i], counter});
+	}
+}
+
+void Day7::printAlignes() const
+{
+	for (const Occurence& o : m_occurences)
+	{
+		std::cout << o.number << " apparait " << o.counter << " fois." << std::endl;
+	}
+}
+
+
+
 void runDay7()
 {
 	Day7 day7("res/day7.txt");
 	std::cout << "La moyenne est de " << day7.getMean() << std::endl;
+	day7.detectAlignes();
+	day7.printAlignes();
 }
--- day7.h
+++ day7.h
@@ -1,8 +1,16 @@
 #pragma once
 #include "commoncpp.h"
+#include <algorithm>
 
-// 1. Calculer la valeur médiane
-// 2. Trouver le nombre commun le plus proche de la médiane
+// 1. Calculer la valeur moyenne
+// 2. Trouver le nombre commun le plus proche de la moyenne
+
+#define MAX_LENGTH 10
+
+typedef struct {
+	uint32_t number;
+	int counter;
+} Occurence;
 
 class Day7
 {
@@ -11,10 +19,13 @@ public:
 	~Day7() {};
 
 	uint32_t getMean() const;
+	void detectAlignes();
+	void printAlignes() const;
 
 private:
-	const char* m_filePath;
+	const char* m_filePath = NULL;
 	std::vector<uint32_t> m_positions;
+	std::vector<Occurence> m_occurences;
 };
 
 
